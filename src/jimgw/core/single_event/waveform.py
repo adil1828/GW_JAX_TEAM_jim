7990db354ef0da80defcef08946dd76c0b5b4afc
adil1828 <adil.imam@unab.cl>
2026-05-25 18:39:58 -0400

    Update waveform.py

--- src/jimgw/core/single_event/waveform.py
+++ src/jimgw/core/single_event/waveform.py
@@ -23,7 +23,7 @@ from ripplegw.waveforms.IMRPhenomD_utils import (
 )
 
 from ripplegw.waveforms.IMRPhenomD_QNMdata import fM_CUT
-from ripplegw.constants import  gt, m_per_Mpc, C, PI # EulerGamma,
+#from ripplegw.constants import  EulerGamma, gt, m_per_Mpc, C, PI 
 from jimgw.core.constants import *
 from ripplegw.typing import Array
 from ripplegw import Mc_eta_to_ms
